05010b0a4fcf33eef8af6c426dccb66b0c333b17
Yevhen Vasylevytskyi <e.vasylevytskyi@gmail.com>
2023-05-16 14:54:08 +0300

    refactor ImageGalleryItem to hooks

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,41 +1,66 @@
-import { Component } from "react";
+import { useState } from "react";
 import Modal from "components/Modal/Modal";
 
 import PropTypes from "prop-types";
 import s from "./ImageGalleryItem.module.css";
 
-class ImageGalleryItem extends Component{
-    
-  state = {
-    isModalOpen: false
-  };  
+function ImageGalleryItem({ item }) {
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
-  openModal = () => this.setState({ isModalOpen: true })
-  closeModal = () => this.setState({ isModalOpen: false })
-
-  render() {
-    const { item } = this.props;
-    const { isModalOpen } = this.state;
-
-    return (
-      <>
-        <li
-          onClick={this.openModal}
-          className={s.ImageGalleryItem}
-        >
-          <img
-            className={s.ImageGalleryItemImage}
-            src={item.webformatURL}
-            alt={item.tags}
-          />
+  const openModal = () => setIsModalOpen(true)
+  const closeModal = () => setIsModalOpen(false)
+  
+  return (
+    <>
+      <li
+        onClick={openModal}
+        className={s.ImageGalleryItem}
+      >
+        <img
+          className={s.ImageGalleryItemImage}
+          src={item.webformatURL}
+          alt={item.tags}
+        />
                   
-        </li>
+      </li>
 
-        {isModalOpen && <Modal onClose={this.closeModal} item={ item } />}
-      </>
-    )
-  }
-};
+      {isModalOpen && <Modal onClose={closeModal} item={ item } />}
+    </>
+  )  
+}
+
+// class ImageGalleryItem extends Component{
+    
+//   state = {
+//     isModalOpen: false
+//   };  
+
+//   openModal = () => this.setState({ isModalOpen: true })
+//   closeModal = () => this.setState({ isModalOpen: false })
+
+//   render() {
+//     const { item } = this.props;
+//     const { isModalOpen } = this.state;
+
+//     return (
+//       <>
+//         <li
+//           onClick={this.openModal}
+//           className={s.ImageGalleryItem}
+//         >
+//           <img
+//             className={s.ImageGalleryItemImage}
+//             src={item.webformatURL}
+//             alt={item.tags}
+//           />
+                  
+//         </li>
+
+//         {isModalOpen && <Modal onClose={this.closeModal} item={ item } />}
+//       </>
+//     )
+//   }
+// };
 
 ImageGalleryItem.protoType = {
   item: PropTypes.object,
