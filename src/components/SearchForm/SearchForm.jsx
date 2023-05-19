41ede74f00e024c1714ebbabbfe6bed974b494e6
Yevhen Vasylevytskyi <e.vasylevytskyi@gmail.com>
2023-05-19 11:13:20 +0300

    refactoring SearchForm to hooks

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,59 +1,102 @@
-import { Component } from "react";
+import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import { toast } from 'react-toastify';
 
 import PropTypes from "prop-types";
 import s from "./SearchForm.module.css";
 
-class SearchForm extends Component{  
+function SearchForm({onSubmit}) {
 
-  state = {
-    searchQuery: "",
-  };
+  const [searchQuery, setSearchQuery] = useState('')
 
-  handleChange = event => {
-    this.setState({ searchQuery: event.currentTarget.value.toLowerCase() })
-    // console.log(this.state.searchQuery)
+  const handleChange = event => {
+    setSearchQuery(event.currentTarget.value.toLowerCase())
   }
 
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
 
-    if (this.state.searchQuery.trim() === "") {
+    if (searchQuery.trim() === "") {
       return toast.error("Введіть пошуковий запит");
     }
-    this.props.onSubmit(this.state.searchQuery);
-    this.setState({ searchQuery: "" });
+    onSubmit(searchQuery);
+    setSearchQuery('');
     }
+
+  return (
+    <form
+      className={s.SearchForm}
+      onSubmit={handleSubmit}
+    >
+      <button
+        type="submit"
+        className={s.SearchFormButton}
+      >                    
+        <FiSearch className={s.SearchSvg} />                    
+      </button>
+
+        <input
+          className={s.SearchFormInput}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={searchQuery}
+          onChange={handleChange}
+        />
+    </form>
+  )
+}
+
+// class SearchForm extends Component{  
+
+//   state = {
+//     searchQuery: "",
+//   };
+
+//   handleChange = event => {
+//     this.setState({ searchQuery: event.currentTarget.value.toLowerCase() })
+//     // console.log(this.state.searchQuery)
+//   }
+
+//   handleSubmit = event => {
+//     event.preventDefault();
+
+//     if (this.state.searchQuery.trim() === "") {
+//       return toast.error("Введіть пошуковий запит");
+//     }
+//     this.props.onSubmit(this.state.searchQuery);
+//     this.setState({ searchQuery: "" });
+//     }
     
-  render() {
+//   render() {
       
     
-        return (
-          <form
-            className={s.SearchForm}
-            onSubmit={this.handleSubmit}
-          >
-              <button
-                type="submit"
-                className={s.SearchFormButton}
-              >                    
-                <FiSearch className={s.SearchSvg} />                    
-              </button>
+//         return (
+//           <form
+//             className={s.SearchForm}
+//             onSubmit={this.handleSubmit}
+//           >
+//               <button
+//                 type="submit"
+//                 className={s.SearchFormButton}
+//               >                    
+//                 <FiSearch className={s.SearchSvg} />                    
+//               </button>
 
-              <input
-                className={s.SearchFormInput}
-                type="text"
-                autoComplete="off"
-                autoFocus
-                placeholder="Search images and photos"
-              value={this.state.searchQuery}
-              onChange={this.handleChange}
-              />
-            </form>
-    )
-  }
-};
+//               <input
+//                 className={s.SearchFormInput}
+//                 type="text"
+//                 autoComplete="off"
+//                 autoFocus
+//                 placeholder="Search images and photos"
+//               value={this.state.searchQuery}
+//               onChange={this.handleChange}
+//               />
+//             </form>
+//     )
+//   }
+// };
 
 SearchForm.protoType = {
   onSubmit: PropTypes.func,
